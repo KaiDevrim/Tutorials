0d005dbc88b21396f5fb2c35f0091f6af502500f
Kai Sakurai <kkgithub@koldkai.com>
2019-11-29 12:34:59 -0800

    yy

---
+++ JavaScript30/myAttempts/01-DrumKit/script.js
@@ -0,0 +1,11 @@
+window.addEventListener('keydown', function(e) {
+    const audio = this.document.querySelector(`audio[data-key="${e.datakey}]"`);
+    const key= this.document.querySelector(`.key[data-key="${e.keyCode}]`);
+    if(!audio) return;
+
+    audio.currentTime = 0;
+    audio.play();
+
+    key.classList.add('playing');
+    
+});
